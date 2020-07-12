4ee62da9ba81b26837b4b1242c24aa75e661c3dc
Fabricio Garcia <gael.fabricio@gmail.com>
2020-07-12 18:37:08 -0500

    feat: Create Footer component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './App.css';
 
 import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
 
 function App() {
-  return <NavBar />;
+  return (
+    <div className="container">
+      <NavBar />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,7 +5,9 @@ function Navbar() {
     <div>
       <nav className="navbar navbar-light bg-dark mb-5">
         <div className="container">
-          <div className="navbar-header">MovieSeriesInfo</div>
+          <div className="navbar-header text-light">
+            MovieSeriesInfo
+          </div>
           <ul className="navbar-nav ml-auto text-light d-inline-block">
             <li className="nav-item d-inline-block mr-4">
               <i className="fab fa-imdb fa-5x" id="imdb-logo" />
