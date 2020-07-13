6a382b1a63060aca7e46f36e327ab55f1eb52f0e
Fabricio <gael.fabricio@gmail.com>
2020-07-13 16:26:49 -0500

    feat: Add fetchMovies action to searcAction.js

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -1,8 +1,21 @@
-import { SEARCH_MOVIE } from './types';
+import axios from 'axios';
+
+import { SEARCH_MOVIE, FETCH_MOVIES } from './types';
+import { OMDbAPIKey } from '../OMDbAPIKey';
 
 export const searchMovie = text => dispatch => {
   dispatch({
     type: SEARCH_MOVIE,
     payload: text,
   });
+};
+
+export const fetchMovies = text => dispatch => {
+  axios
+    .get(`http://www.omdbapi.com/?apikey=${OMDbAPIKey}&s=${text}`)
+    .then(response => dispatch({
+      type: FETCH_MOVIES,
+      payload: response.data,
+    }))
+    .catch(error => console.log(error));
 };
