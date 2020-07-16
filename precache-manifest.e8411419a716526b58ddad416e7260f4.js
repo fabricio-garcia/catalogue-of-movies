00000e0b58916ea7ca4e1afd8466c38c6a0056b0
Fabricio <gael.fabricio@gmail.com>
2020-07-15 19:45:46 -0500

    Updates

---
+++ precache-manifest.e8411419a716526b58ddad416e7260f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7d15b31214b9a9642e08a08b240042a",
+    "url": "/catalogue-of-movies/index.html"
+  },
+  {
+    "revision": "49f0eb4226701ae57930",
+    "url": "/catalogue-of-movies/static/css/main.0698c0a6.chunk.css"
+  },
+  {
+    "revision": "147b685faa361b98accb",
+    "url": "/catalogue-of-movies/static/js/2.c9590001.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/catalogue-of-movies/static/js/2.c9590001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49f0eb4226701ae57930",
+    "url": "/catalogue-of-movies/static/js/main.d76c4b6e.chunk.js"
+  },
+  {
+    "revision": "44ff0ae8c72b39d32c20",
+    "url": "/catalogue-of-movies/static/js/runtime-main.a426cdcc.js"
+  }
+]);
